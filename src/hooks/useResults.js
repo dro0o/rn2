f8368beb5dc20c415ce346bdf2532e53cd0ef5ca
Andrew Pittman <andrew@pittman.dev>
2020-03-09 22:55:15 -0400

    make yelp a custom hook

---
+++ src/hooks/useResults.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import yelp from '../api/yelp'
+
+export default () => {
+  const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term: searchTerm,
+          location: 'washington dc',
+          limit: 50
+        }
+      })
+      setResults(response.data.businesses)
+    } catch (err) {
+      setErrorMessage('Something went HORRIBLY WRONG!?!?!?!')
+    }
+  }
+
+  // Call searchApi when component is first rendered
+  useEffect(() => {
+    searchApi('pasta')
+  }, [])
+
+  return [searchApi, results, errorMessage]
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
-  const [results, setResults] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
-
-  const searchApi = async searchTerm => {
-    console.log('hi watup!')
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          term: searchTerm,
-          location: 'washington dc',
-          limit: 50
-        }
-      })
-      setResults(response.data.businesses)
-    } catch (err) {
-      setErrorMessage('Something went HORRIBLY WRONG!?!?!?!')
-    }
-  }
-
-  // Call searchApi when component is first rendered
-  useEffect(() => {
-    searchApi('pasta')
-  }, [])
+  const [searchApi, results, errorMessage] = useResults()
 
   return (
     <View>
@@ -38,6 +18,9 @@ const SearchScreen = () => {
       />
       {errorMessage !== '' ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results!</Text>
+      <ResultsList />
+      <ResultsList />
+      <ResultsList />
     </View>
   )
 }
